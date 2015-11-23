054443b5422c8becd2aa94ddf19404bf2330711c
Aaron Carson <acarson13@mail.wou.edu>
2015-11-23 11:54:50 -0800

    Added out first interface ideas to oled.h

--- src/oled.h
+++ src/oled.h
@@ -11,7 +11,60 @@
 #include <unistd.h>
 
 
-void test();
+/**
+ * A temporary test method to ensure our setup works.
+ */
+void oled_test();
+
+
+/**
+ * Initialize all memory needed
+ */
+void oled_init();
+
+/**
+ * Turns the OLED screen on, and displays the current info in the buffer.
+ */
+void oled_enable();
+
+/**
+ * Turns the OLED screen off, but saves the data in the buffer.,
+ */
+void oled_disable();
+
+/**
+ * Accepts arguments equivalent to printf and treats the OLED display as a display.
+ * @param args The format string and optional arguments.
+ */
+void oled_printf(const char * args, ...);
+
+/**
+ * Free all allocated memory and release all resources.
+ */
+void oled_close();
+
+/**
+ * Clear the screen buffer.  Call this any time you want to display something on a blank
+ * background.
+ */
+void oled_clear();
+
+/**
+ * Not sure yet.
+ */
+void oled_append();
+
+/**
+ * Update the OLED display with the current screen buffer.
+ */
+void oled_update();
+
+
+/**
+ * @return The text currently in the buffer.
+ */
+char * oled_getBuffer();
+
 
 
 #endif /* WOU_3D_SCANNER_OLED_H */
