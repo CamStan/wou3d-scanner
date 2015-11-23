bea997b96637f502ff7776ed5c2c1d5bf6b981b9
Aaron Carson <acarson13@mail.wou.edu>
2015-11-23 10:52:03 -0800

    Added sparkfun source code, and linked up oled.cpp and oled.h with the source code, and added a test method to show the OLED display works with our setup.

---
+++ src/oled.h
@@ -0,0 +1,17 @@
+/**
+ * @file oled.h
+ * @brief Control the OLED display: initialize, enable, disable, display certain things, close.
+ *
+ * This is designed to be a simple interface for controling the Edison OLED display.
+ */
+
+#ifndef WOU_3D_SCANNER_OLED_H
+#define WOU_3D_SCANNER_OLED_H
+#include "sparkfun/oled/Edison_OLED.h"
+#include <unistd.h>
+
+
+void test();
+
+
+#endif /* WOU_3D_SCANNER_OLED_H */
