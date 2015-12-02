acac250d9661adf0820eb85a8d9a6d328cde56b3
Aaron Carson <acarson13@mail.wou.edu>
2015-12-02 10:41:41 -0800

    Finished writing our oled.h header file.

--- src/oled.h
+++ src/oled.h
@@ -1,21 +1,18 @@
 /**
  * @file oled.h
- * @brief Control the OLED display: initialize, enable, disable, display certain things, close.
+ * @brief Control the OLED display: initialize, enable, disable, display
+ * certain things, close.
  *
- * This is designed to be a simple interface for controling the Edison OLED display.
+ * This is designed to be a simple interface for controlling the Edison OLED
+ * display.
+ *
+ * @author Aaron Carson, Joseph Shin, Jason Gersztyn
  */
 
 #ifndef WOU_3D_SCANNER_OLED_H
 #define WOU_3D_SCANNER_OLED_H
 #include "sparkfun/oled/Edison_OLED.h"
 #include <unistd.h>
-
-
-/**
- * A temporary test method to ensure our setup works.
- */
-void oled_test();
-
 
 /**
  * Initialize all memory needed
@@ -28,12 +25,13 @@ void oled_init();
 void oled_enable();
 
 /**
- * Turns the OLED screen off, but saves the data in the buffer.,
+ * Turns the OLED screen off, but saves the data in the buffer.
  */
 void oled_disable();
 
 /**
- * Accepts arguments equivalent to printf and treats the OLED display as a display.
+ * Accepts arguments equivalent to printf and treats the OLED display as a
+ * display.
  * @param args The format string and optional arguments.
  */
 void oled_printf(const char * args, ...);
@@ -44,27 +42,27 @@ void oled_printf(const char * args, ...);
 void oled_close();
 
 /**
- * Clear the screen buffer.  Call this any time you want to display something on a blank
+ * Clear the screen buffer.  Call this any time you want to display something
+ * on a blank
  * background.
  */
 void oled_clear();
-
-/**
- * Not sure yet.
- */
-void oled_append();
 
 /**
  * Update the OLED display with the current screen buffer.
  */
 void oled_update();
 
-
 /**
  * @return The text currently in the buffer.
  */
 char * oled_getBuffer();
 
-
+/**
+ * Set the cursor position based on font width and height, not by pixel.
+ *  @param column The horizontal cursor position.
+ *  @param row    The vertical cursor position.
+ */
+void oled_setCursor(int column, int row);
 
 #endif /* WOU_3D_SCANNER_OLED_H */
