69a421426926de47b362191abc0ffcb75b64ed86
Gene Osborne <gene.osborne@gmail.com>
2015-12-02 12:33:31 -0800

    Updated Logic for example program to known solution

--- examples/buttonExample.c
+++ examples/buttonExample.c
@@ -9,20 +9,21 @@ void closeButtons();
 struct button_context * buttonA;
 struct button_context * buttonB;
 struct button_context * buttonSelect;
-
+int flag;
 int main() {
 	/* Setup your example here, code that should run once
 	 */
 	buttonA = button_init(PIN_A, &f1);
 	buttonB = button_init(PIN_B, &f2);
 	buttonSelect = button_init(PIN_SELECT, &closeButtons);
-
+	flag = 0;
 	/* Code in this loop will run repeatedly
 	 */
-	while(buttonSelect != NULL) {
+	while(flag == 0) {
 		//contiue until close()
 	}
-
+	button_close(buttonSelect);
+	printf("Select closed\n");
 	return 0;
 }
 
@@ -37,7 +38,8 @@ void f2(){
 void closeButtons(){
 	printf("Select button pressed\n");
 	button_close(buttonA);
+	printf("A closed\n");
 	button_close(buttonB);
-	button_close(buttonSelect);
-	buttonSelect = NULL;
+	printf("B closed\n");
+	flag = 1;
 }
