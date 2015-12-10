08c6f01f69cd8ba466cfeff16d28632ce8e122ed
root <root@M-SILVER-ROGUE.vpn.nightsongdesigns.com>
2015-12-10 15:19:35 -0800

    Minor discrepencies caused minor rewrite due to nix make errors

--- src/display7seg.c
+++ src/display7seg.c
@@ -9,6 +9,7 @@
  * through uart serial control on a single pin.
  */
 #include "display7seg.h"
+#include "mraa.h"
 #include <stdint.h>
 
 /*----- DEFINES SECTION -----*/
@@ -21,7 +22,7 @@
 
 /***VARS***/
 static char display[4];
-static uint8_t DEC_CONTROL[2];
+static char decimal_control[2];
 static uint8_t err;
 static mraa_uart_context uart;
 
@@ -31,7 +32,7 @@ Reset
 Sends factory RESET code to the display
 */
 void display7seg_reset(){
-	mraa_uart_write(uart, RESET);
+	mraa_uart_write(uart, (char*) RESET, sizeof(RESET));
 }
 /**
 Clear Screen
@@ -39,7 +40,7 @@ Clear Screen
 Sends the CLEAR screen command to the display
 */
 void display7seg_clear(){
-	mraa_uart_write(uart, CLEAR);
+	mraa_uart_write(uart, (char*) CLEAR, sizeof(CLEAR));
 }
 /**
 Init with custom BAUD
@@ -51,10 +52,11 @@ default baud rate.
 @param rx The Serial rx pin to use
 @param BAUD Override for the BAUD rate
 */
-void display7seg_init(uint8_t rx, uint8_t baud){
-	uart = mraa_uart_init(rx);
-	mraa_uart_set_baudrate(uart, baud);
-}
+//***----- Nix MAKE does not like overload, assuming this is a C89 vs C9x issue -----***//
+//void display7seg_init(uint8_t rx, uint8_t baud){
+//	uart = mraa_uart_init(rx);
+//	mraa_uart_set_baudrate(uart, baud);
+//}
 /**
 Initializer
 
@@ -80,7 +82,6 @@ code ability.
 */
 void display7seg_setD1(char d){
 	display[0] = d;
-	display7seg_setDisplay(display);
 }
 /**
 Set Digit 2
@@ -94,7 +95,6 @@ code ability.
 */
 void display7seg_setD2(char d){
 	display[1] = d;
-	display7seg_setDisplay(display);
 }
 /**
 Set Digit 3
@@ -108,7 +108,6 @@ code ability.
 */
 void display7seg_setD3(char d){
 	display[2] = d;
-	display7seg_setDisplay(display);
 }
 /**
 Set Digit 4
@@ -122,7 +121,6 @@ code ability.
 */
 void display7seg_setD4(char d){
 	display[3] = d;
-	display7seg_setDisplay(display);
 }
 /**
 Set Brightness of display
@@ -132,8 +130,8 @@ to 255.
 
 @param  Brightness value to set brightness
 */
-void display7seg_setBright(uint8_t Brightness){
-	display7seg_sendCmd(BRIGHT_CONT, Brightness);
+void display7seg_setBright(char Brightness){
+	display7seg_sendCmd((char) BRIGHT_CONT, Brightness);
 }
 
 /**
@@ -148,15 +146,14 @@ screen as " Err" for easy debugging.
 
 @param  on Value of on or off
 */
-void display7seg_setDecimal(uint8_t on){
+void display7seg_setDecimal(char on){
 	if(on == 1){
-		DEC_CONTROL[0] = 0x10;
+		decimal_control[0] = 0x10;
 	}
 	if(on == 0){
-		DEC_CONTROL[0] = 0x00;
+		decimal_control[0] = 0x00;
 	}
 	if(on <= -1 || on >= 1){
-		// Go to hell
 		display7seg_setErrorState(1);
 	}
 }
@@ -172,12 +169,12 @@ screen as " Err" for easy debugging.
 
 @param  on Value of on or off
 */
-void display7seg_setDegree(uint8_t on){
+void display7seg_setDegree(char on){
 	if(on == 1){
-		DEC_CONTROL[0] = 0x20;
+		decimal_control[1] = 0x20;
 	}
 	if(on == 0){
-		DEC_CONTROL[0] = 0x00;
+		decimal_control[1] = 0x00;
 	}
 	if(on <= -1 || on >= 1){
 		//Set the error state if incorrect boolean found
@@ -195,9 +192,9 @@ new functionality in the future.
 @param  cmdType The value of the command type to be sent to the display
 @param cmd The actual command to be sent.
 */
-void display7seg_sendCmd(uint8_t cmdType, uint8_t cmd){
-	mraa_uart_write(uart,cmdType);
-	mraa_uart_write(uart,cmd);
+void display7seg_sendCmd(char cmdType, char cmd){
+	mraa_uart_write(uart, &cmdType,sizeof(cmdType));
+	mraa_uart_write(uart, &cmd,sizeof(cmd));
 }
 /**
 Display
@@ -207,12 +204,13 @@ the triggers for decimal and degree and sets
 the display accordingly to show the information.
 */
 void display7seg_Display(){
-	mraa_uart_write(uart,display[0]);
-	mraa_uart_write(uart,display[1]);
-	mraa_uart_write(uart,display[2]);
-	mraa_uart_write(uart,display[3]);
-	mraa_uart_write(uart,DEC_CONTROL);
-	mraa_uart_write(uart,(DEC_CONTROL[0] | DEC_CONTROL[1]));
+	mraa_uart_write(uart,&display[0],sizeof(display[0]));
+	mraa_uart_write(uart,&display[1],sizeof(display[1]));
+	mraa_uart_write(uart,&display[2],sizeof(display[2]));
+	mraa_uart_write(uart,&display[3],sizeof(display[3]));
+	mraa_uart_write(uart,(char*) DEC_CONTROL,sizeof(DEC_CONTROL));
+	char dec = decimal_control[0] | decimal_control[1];
+	mraa_uart_write(uart,&dec,sizeof(dec));
 }
 
 /**
@@ -242,8 +240,8 @@ void display7seg_error(){
 		display[1] = 'E';
 		display[2] = 'r';
 		display[3] = 'r';
-		DEC_CONTROL[0] = 0x00;
-		DEC_CONTROL[1] = 0x00;
+		decimal_control[0] = 0x00;
+		decimal_control[1] = 0x00;
 		display7seg_Display();
 	}
 }
@@ -255,5 +253,6 @@ described in the MRAA library.
 */
 void display7seg_close(){
 		mraa_uart_stop(uart);
-		mraa_uart_deinit();
+		//De-Init deprecated no longer in MRAA lib or needed.	
+		//mraa_uart_deinit();
 }
